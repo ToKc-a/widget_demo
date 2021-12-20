6458f8408aebcb7ea18fee93dc5bbdadb1172ebc
ToKc <ant.tokc@gmail.com>
2021-12-20 17:17:59 +0200

    update

--- lib/navigation_drawer_screen.dart
+++ lib/navigation_drawer_screen.dart
@@ -23,22 +23,21 @@ class _NavigationDrawerScreenState extends State<NavigationDrawerScreen> {
                   color: Colors.blue,
                 ),
                 child: Container(
-                  height: 200,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
+                    height: 200,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
                       Container(
-                        height: 100,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(50.0))
-                        ),
-                        child: Image.network("https://upload.wikimedia.org/wikipedia/commons/4/44/Google-flutter-logo.svg")
+                      height: 100,
+                      decoration: BoxDecoration(
+                          borderRadius:
+                          BorderRadius.all(Radius.circular(50.0))),
+                      child: SvgPicture.network("https://upload.wikimedia.org/wikipedia/commons/4/44/Google-flutter-logo.svg"),
                       ),
-                      Text("Навигация во Flutter")
-                    ],
-                  )
-                ),
+                        Text("Навигация во Flutter"),
+                      ],
+                    )),
               ),
               ListTile(
                 leading: Icon(Icons.one_k),
@@ -55,10 +54,8 @@ class _NavigationDrawerScreenState extends State<NavigationDrawerScreen> {
         ),
         body: Center(
           child: Text("Содержимое экрана"),
-        )
-        ,
-      )
-      ,
+        ),
+      ),
     );
   }
 }
