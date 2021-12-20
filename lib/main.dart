2c1b4885339e1a61f1fe299fd748f670022c2938
ToKc <ant.tokc@gmail.com>
2021-12-20 16:59:10 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:widget_demo/navigation_drawer_screen.dart';
 import 'package:widget_demo/switches_demo_script.dart';
 import 'package:widget_demo/text_demo_screen.dart';
 
 void main() {
-  runApp(const SwitchDemoScreen());
+  runApp(const NavigationDrawerScreen());
 }
--- lib/navigation_drawer_screen.dart
+++ lib/navigation_drawer_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class NavigationDrawerScreen extends StatefulWidget {
   const NavigationDrawerScreen({Key? key}) : super(key: key);
@@ -10,6 +11,54 @@ class NavigationDrawerScreen extends StatefulWidget {
 class _NavigationDrawerScreenState extends State<NavigationDrawerScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Container(
+                  height: 200,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(50.0))
+                        ),
+                        child: Image.network("https://upload.wikimedia.org/wikipedia/commons/4/44/Google-flutter-logo.svg")
+                      ),
+                      Text("Навигация во Flutter")
+                    ],
+                  )
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.one_k),
+                title: Text('Каталог'),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.two_k),
+                title: Text('Корзина'),
+                onTap: () {},
+              )
+            ],
+          ),
+        ),
+        body: Center(
+          child: Text("Содержимое экрана"),
+        )
+        ,
+      )
+      ,
+    );
   }
 }
