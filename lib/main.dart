b33bb8c28417dd4fa0f5a271a5312bac80662f46
ToKc <ant.tokc@gmail.com>
2021-12-22 14:22:10 +0200

    Add AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:widget_demo/app_bar_screen.dart';
 import 'package:widget_demo/navigation_drawer_screen.dart';
 import 'package:widget_demo/switches_demo_script.dart';
 import 'package:widget_demo/text_demo_screen.dart';
 
 void main() {
-  runApp(const NavigationDrawerScreen());
+  runApp(const AppBarScreen());
 }
